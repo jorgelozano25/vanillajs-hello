88d3fd845688ecda3b85419124158f102fa0c983
jorgelozano25 <jorge_9dealmeria@hotmail.com>
2024-07-17 11:32:11 +0000

    ExcuseGenerator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["El perro", "Mi abuela", "El cartero", "Mi pajaro", "Mi gato"];
+  let action = ["se comio", "orinó", "ralló", "rompio", "enterró"];
+  let what = [
+    "los deberes",
+    "el telefono",
+    "el coche",
+    "las gafas",
+    "la alfombra"
+  ];
+  let when = [
+    "antes de clase",
+    "cuando estaba durmiendo",
+    "mientras hacia mi ejercicio",
+    "durante el almuerzo",
+    "mientras estaba rezando"
+  ];
+
+  function generateExcuse() {
+    let randomWho = Math.floor(Math.random() * who.length);
+    let randomAction = Math.floor(Math.random() * action.length);
+    let randomWhat = Math.floor(Math.random() * what.length);
+    let randomWhen = Math.floor(Math.random() * when.length);
+    let excuse =
+      who[randomWho] +
+      " " +
+      action[randomAction] +
+      " " +
+      what[randomWhat] +
+      " " +
+      when[randomWhen];
+    return excuse;
+  }
+
+  generateExcuse();
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
